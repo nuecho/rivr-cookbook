e03831fe2392d7b17be0870680b0a6b6b45539d0
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-16 09:00:23 -0400

    Added base for messages recipes.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -8,6 +8,15 @@ import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
 
+/**
+ * A message turn is a primitive to <i>queue</i> a message on the VoiceXML
+ * platform. The message is not played immediately, but rather when the VoiceXML
+ * platform decides to do so.
+ * 
+ * @see <a href="http://www.w3.org/TR/voicexml20/#dml4.1.8">VoiceXML
+ *      specification, 4.1.8 Prompt Queueing and Input Collection</a>
+ * @author Nu Echo Inc.
+ */
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
