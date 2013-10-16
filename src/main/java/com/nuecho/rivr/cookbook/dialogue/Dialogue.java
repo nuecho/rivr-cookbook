90fa71c97ac99cca85d687ce6df20c8069bfbce2
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-16 09:05:17 -0400

    Added blind transfer example.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,15 +4,28 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        //Bind transfer
+        BlindTransfer blindTransfer = new BlindTransfer("blind-transfer", "sip:test@example.com");
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(blindTransfer, context);
+
+        if (VoiceXmlEvent.hasEvent(VoiceXmlEvent.CONNECTION_DISCONNECT_TRANSFER, inputTurn.getEvents())) {
+            context.getLogger().info("Transfer done");
+        } else {
+            context.getLogger().info("Something went wrong with the transfer.");
+        }
 
         //end of dialogue
         return new Exit("exit");
