0759e2487dccf303fefd1d25674f0a33b9893cfa
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-16 23:02:27 -0400

    Added example for specifying the language of a message

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,12 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 /**
  * A message turn is a primitive to <i>queue</i> a message on the VoiceXML
@@ -22,6 +25,24 @@ public class Dialogue implements VoiceXmlDialogue {
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        //Play a synthesis message in another language
+        Message message = new Message("synthesis-french-message",
+                                      new SpeechSynthesis("Ceci est un message."));
+        message.setLanguage("fr-CA");
+        DialogueUtils.doTurn(message, context);
+
+        //Alternatively, you can change the default VoiceXML language. This is done in the context.
+        //This will change the language for all up coming message for which no language has been set. 
+        context.setLanguage("fr-CA");
+
+        Message otherMessage = new Message("synthesis-french-message",
+                                           new SpeechSynthesis("Ceci est un autre message."));
+        DialogueUtils.doTurn(otherMessage, context);
+
+        Message yetAnotherMessage = new Message("synthesis-french-message",
+                                                new SpeechSynthesis("Et encore un."));
+        DialogueUtils.doTurn(yetAnotherMessage, context);
 
         //end of dialogue
         return new Exit("exit");
