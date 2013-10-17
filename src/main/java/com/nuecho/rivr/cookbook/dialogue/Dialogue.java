f19de10285ae3ffd0cacae601630bec22325e4c3
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-16 23:18:52 -0400

    Added hello world example.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,18 +4,24 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import static com.nuecho.rivr.core.dialogue.DialogueUtils.*;
+
+import com.nuecho.rivr.core.channel.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
-            throws Exception {
+            throws Timeout, InterruptedException {
 
-        //end of dialogue
+        Message message = new Message("message", new SpeechSynthesis("Hello World!"));
+        doTurn(message, context);
+
         return new Exit("exit");
     }
-
 }
