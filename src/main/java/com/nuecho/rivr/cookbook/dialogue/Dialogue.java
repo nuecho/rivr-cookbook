555c903cdf97a57b11169b979ddb3126b35158bb
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-18 08:28:19 -0400

    Added recording recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,21 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import static com.nuecho.rivr.voicexml.turn.output.OutputTurns.*;
+
+import java.io.*;
+
+import org.slf4j.*;
+
+import com.nuecho.rivr.core.dialogue.*;
+import com.nuecho.rivr.core.util.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.servlet.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 /**
  * The goal of the <code>Interaction</code> is to collect information from the
@@ -43,6 +55,43 @@ public class Dialogue implements VoiceXmlDialogue {
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        Recording recording = new Recording();
+        recording.setBeep(true);
+        recording.setPostAudioToServer(true);
+        recording.setType("audio/x-wav");
+        recording.setFinalSilence(Duration.seconds(2));
+        recording.setDtmfTerm(true);
+        recording.setMaximumTime(Duration.seconds(20));
+
+        SpeechSynthesis synthesisText = new SpeechSynthesis("Say your message after the beep.");
+        Interaction interaction = interaction("record")
+                .addPrompt(synthesisText)
+                .build(recording, Duration.seconds(5));
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(interaction, context);
+
+        Logger logger = context.getLogger();
+        if (inputTurn.getRecordingInfo() != null) {
+            RecordingInfo recordingInfo = inputTurn.getRecordingInfo();
+            logger.info("DTMF term char: " + recordingInfo.getDtmfTerm());
+            logger.info("Maximum time reached: " + recordingInfo.isMaxTime());
+            logger.info("Recording duration: " + recordingInfo.getDuration());
+
+            FileUpload file = recordingInfo.getFile();
+            logger.info("Recording filename: " + file.getName());
+            logger.info("Recording type: " + file.getContentType());
+            logger.info("Recording size: " + file.getContent().length);
+
+            File temporaryDirectory = new File(System.getProperty("java.io.tmpdir"));
+            File outputFile = new File(temporaryDirectory, "recording.wav");
+            logger.info("Writing audio file to: " + outputFile.getAbsolutePath());
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(file.getContent());
+            outputStream.close();
+        } else if (VoiceXmlEvent.hasEvent(VoiceXmlEvent.NO_INPUT, inputTurn.getEvents())) {
+            logger.info("Timeout.");
+        }
 
         //end of dialogue
         return new Exit("exit");
