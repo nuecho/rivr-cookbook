56fdc3761f49ca31e51fd0c7c3c6d69dd7c20de9
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-24 21:28:15 -0400

    Added subdialogue implementation recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,18 +4,59 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import javax.json.*;
+
+import com.nuecho.rivr.core.channel.*;
+import com.nuecho.rivr.core.dialogue.*;
+import com.nuecho.rivr.core.util.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.rendering.voicexml.*;
+import com.nuecho.rivr.voicexml.turn.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
+import com.nuecho.rivr.voicexml.turn.output.grammar.*;
+import com.nuecho.rivr.voicexml.turn.output.interaction.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
-    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context) throws Exception {
+    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
+            throws Exception {
 
-        //end of dialogue
-        return new VoiceXmlExitTurn("exit");
+        try
+        {
+            int number1 = askNumber(context);
+            int number2 = askNumber(context);
+
+            VariableDeclarationList returnValues = new VariableDeclarationList();
+            returnValues.addVariable(new VariableDeclaration("number1", String.valueOf(number1)));
+            returnValues.addVariable(new VariableDeclaration("number2", String.valueOf(number2)));
+            returnValues.addVariable(new VariableDeclaration("sum", String.valueOf(number1 + number2)));
+
+            //return the values
+            return new VoiceXmlReturnTurn("return", returnValues);
+        } catch (Exception exception) {
+            //return an event
+            return new VoiceXmlReturnTurn("return", "rivr.cookbook.error", exception.getMessage());
+        }
     }
 
+    private int askNumber(VoiceXmlDialogueContext context) throws Timeout, InterruptedException, Exception {
+        DtmfRecognitionConfiguration configuration =
+                new DtmfRecognitionConfiguration(new GrammarReference("builtin:dtmf/number"));
+
+        InteractionBuilder builder = InteractionBuilder.newInteractionBuilder("interaction");
+        InteractionTurn interaction = builder.addPrompt(configuration, new SynthesisText("Enter a number."))
+                                             .build(configuration, TimeValue.seconds(5));
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(context, interaction);
+
+        if (inputTurn.getRecognitionInfo() == null) throw new Exception("Unexpected result");
+
+        JsonArray recognitionResult = inputTurn.getRecognitionInfo().getRecognitionResult();
+        String number = recognitionResult.getJsonObject(0).getString("interpretation");
+        return Integer.parseInt(number);
+    }
 }
