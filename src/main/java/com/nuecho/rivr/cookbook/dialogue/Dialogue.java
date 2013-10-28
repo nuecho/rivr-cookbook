209e2de60e7bc7a70a964c5b9034b8e9523fb4a5
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-28 15:33:46 -0400

    Added subdialogue implementation (with VoiceXML parameters) recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -26,9 +26,21 @@ public class Dialogue implements VoiceXmlDialogue {
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
 
+        VariableList parameters = new VariableList();
+        parameters.add("promptText1");
+        parameters.addWithString("promptText2", "Enter a number");
+
+        Script script = new Script("get-parameters");
+        script.setVariables(parameters);
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(script, context);
+        JsonObject parameterValues = (JsonObject) inputTurn.getJsonValue();
+        String promptText1 = parameterValues.getString("promptText1");
+        String promptText2 = parameterValues.getString("promptText2");
+
         try {
-            int number1 = askNumber(context);
-            int number2 = askNumber(context);
+            int number1 = askNumber(context, promptText1);
+            int number2 = askNumber(context, promptText2);
 
             VariableList returnValues = new VariableList();
             returnValues.addWithExpression("number1", String.valueOf(number1));
@@ -43,11 +55,12 @@ public class Dialogue implements VoiceXmlDialogue {
         }
     }
 
-    private int askNumber(VoiceXmlDialogueContext context) throws Timeout, InterruptedException, Exception {
+    private int askNumber(VoiceXmlDialogueContext context, String promptText) throws Timeout,
+            InterruptedException, Exception {
         DtmfRecognition configuration = new DtmfRecognition(new GrammarReference("builtin:dtmf/number"));
 
         Interaction interaction = interaction("interaction")
-                .addPrompt(configuration, new SpeechSynthesis("Enter a number."))
+                .addPrompt(configuration, new SpeechSynthesis(promptText))
                 .build(configuration, Duration.seconds(5));
 
         VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(interaction, context);
