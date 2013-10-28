d13f4182ad240f8f026332f39b166a42757a0e49
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-28 15:33:19 -0400

    Added GVP uuid recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,15 +4,32 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import javax.json.*;
+
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.turn.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        VariableList variables = new VariableList();
+        variables.addWithExpression("uuid", "session.connection.uuid");
+
+        Script script = new Script("get-uuid");
+        script.setVariables(variables);
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(script, context);
+
+        JsonObject result = (JsonObject) inputTurn.getJsonValue();
+        context.getLogger().info("UUID: " + result.getString("uuid"));
 
         //end of dialogue
         return new Exit("exit");
