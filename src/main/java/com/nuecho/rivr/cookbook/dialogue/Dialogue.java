73f0feaa85920451698e8db47aa4e58416378da9
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-28 15:32:49 -0400

    Added CLID and DNIS extraction.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,15 +4,35 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import javax.json.*;
+
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.turn.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        VariableList variables = new VariableList();
+        variables.addWithExpression("clid", "session.connection.remote.uri");
+        variables.addWithExpression("dnis", "session.connection.local.uri");
+
+        Script script = new Script("clid-dnis");
+        script.setVariables(variables);
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(script, context);
+
+        JsonObject object = (JsonObject) inputTurn.getJsonValue();
+        String clid = object.getString("clid");
+        String dnis = object.getString("dnis");
+        context.getLogger().info("CLID: {}, DNIS: {}", clid, dnis);
 
         //end of dialogue
         return new Exit("exit");
