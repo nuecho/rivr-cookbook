a2bbdc311a32d0ee55df073f0c29a5d7e02eea94
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-29 16:31:32 -0400

    Added subdialogue implementation recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,21 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import static com.nuecho.rivr.voicexml.turn.output.OutputTurns.*;
+
+import javax.json.*;
+
+import com.nuecho.rivr.core.channel.*;
+import com.nuecho.rivr.core.dialogue.*;
+import com.nuecho.rivr.core.util.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.turn.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
+import com.nuecho.rivr.voicexml.turn.output.grammar.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
@@ -14,8 +26,36 @@ public class Dialogue implements VoiceXmlDialogue {
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
 
-        //end of dialogue
-        return new Exit("exit");
+        try {
+            int number1 = askNumber(context);
+            int number2 = askNumber(context);
+
+            VariableList returnValues = new VariableList();
+            returnValues.addWithExpression("number1", String.valueOf(number1));
+            returnValues.addWithExpression("number2", String.valueOf(number2));
+            returnValues.addWithExpression("sum", String.valueOf(number1 + number2));
+
+            //return the values
+            return new Return("return", returnValues);
+        } catch (Exception exception) {
+            //return an event
+            return new Return("return", "rivr.cookbook.error", exception.getMessage());
+        }
     }
 
+    private int askNumber(VoiceXmlDialogueContext context) throws Timeout, InterruptedException, Exception {
+        DtmfRecognition configuration = new DtmfRecognition(new GrammarReference("builtin:dtmf/number"));
+
+        Interaction interaction = interaction("interaction")
+                .addPrompt(configuration, new SpeechSynthesis("Enter a number."))
+                .build(configuration, Duration.seconds(5));
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(interaction, context);
+
+        if (inputTurn.getRecognitionInfo() == null) throw new Exception("Unexpected result");
+
+        JsonArray recognitionResult = inputTurn.getRecognitionInfo().getRecognitionResult();
+        String number = recognitionResult.getJsonObject(0).getString("interpretation");
+        return Integer.parseInt(number);
+    }
 }
