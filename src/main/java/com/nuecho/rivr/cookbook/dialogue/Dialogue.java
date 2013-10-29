7a4c36fcf4d094e0cc869d7b444b7a66979d71e1
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-29 15:59:01 -0400

    Added alternate synthesis text example.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,12 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 /**
  * A message turn is a primitive to <i>queue</i> a message on the VoiceXML
@@ -23,8 +26,14 @@ public class Dialogue implements VoiceXmlDialogue {
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
 
+        //Play an audio file with alternate content
+        String recordingLocation = context.getContextPath() + "/audio/test.wav";
+        String alternateText = "This message is played because the audio file could not be fetched";
+        AudioFile audioFile = AudioFile.fromLocation(recordingLocation, alternateText);
+        Message message = new Message("recording-message", audioFile);
+        DialogueUtils.doTurn(message, context);
+
         //end of dialogue
         return new Exit("exit");
     }
-
 }
