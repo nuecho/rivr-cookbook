5775570bcd55f4414f944ead36cb18ea1f99e117
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-10-29 16:31:14 -0400

    Added hello world 2 example.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,18 +4,62 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import static com.nuecho.rivr.voicexml.turn.input.VoiceXmlEvent.*;
+
+import java.util.*;
+
+import javax.json.*;
+
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.turn.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
+import com.nuecho.rivr.voicexml.util.*;
+import com.nuecho.rivr.voicexml.util.json.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
-    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
-            throws Exception {
+    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context) {
 
-        //end of dialogue
-        return new Exit("exit");
+        JsonObjectBuilder resultObjectBuilder = JsonUtils.createObjectBuilder();
+        String status;
+
+        try {
+            // Play a prompt
+            Message message = new Message("message", new SpeechSynthesis("Hello World!"));
+            VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(message, context);
+
+            // Handling hangup or error events
+            List<VoiceXmlEvent> events = inputTurn.getEvents();
+            if (events.isEmpty()) {
+                status = "Normal";
+            } else if (hasEvent(CONNECTION_DISCONNECT_HANGUP, events)) {
+                status = "HangUp";
+            } else if (hasEvent(ERROR, events)) {
+                status = "PlatformError";
+                VoiceXmlEvent errorEvent = getEvent(ERROR, events);
+                resultObjectBuilder.add("eventName", errorEvent.getName());
+                resultObjectBuilder.add("eventMessage", errorEvent.getMessage());
+            } else {
+                status = "Unknown";
+            }
+        } catch (InterruptedException exception) {
+            Thread.currentThread().interrupt();
+            status = "Interrupted";
+        } catch (Throwable throwable) {
+            status = "SystemError";
+            context.getLogger().error("Error during dialogue execution", throwable);
+            resultObjectBuilder.add("error", ResultUtils.toJson(throwable));
+        }
+
+        resultObjectBuilder.add("status", status);
+        VariableList variables = VariableList.create(resultObjectBuilder.build());
+
+        return new Exit("result", variables);
     }
-
 }
