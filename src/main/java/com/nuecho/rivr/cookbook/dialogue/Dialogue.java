8f1e3d5e540e155cba7543c52a066c0942d6a6ad
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-11-11 14:09:06 -0500

    Added example for multiple audio items.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,12 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 /**
  * A message turn is a primitive to <i>queue</i> a message on the VoiceXML
@@ -22,6 +25,13 @@ public class Dialogue implements VoiceXmlDialogue {
     @Override
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
+
+        //Play a speech synthesis message and an audio file
+        String recordingLocation = context.getContextPath() + "/audio/test.wav";
+        Message message = new Message("mixed-messages",
+                                      new SpeechSynthesis("This is a message"),
+                                      AudioFile.fromLocation(recordingLocation));
+        DialogueUtils.doTurn(message, context);
 
         //end of dialogue
         return new Exit("exit");
