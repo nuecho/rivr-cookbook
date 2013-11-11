b3f1db4131ec15ea56d30093454c973fa524c893
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2013-11-11 15:46:16 -0500

    Added subdialogue invocation recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,9 +4,17 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import java.util.Map.Entry;
+
+import javax.json.*;
+
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
+import com.nuecho.rivr.voicexml.rendering.voicexml.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
+import com.nuecho.rivr.voicexml.turn.input.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
@@ -14,8 +22,25 @@ public class Dialogue implements VoiceXmlDialogue {
     public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
             throws Exception {
 
+        SubdialogueCall subdialogueCall = new SubdialogueCall("invoke-subdialogue", "subdialogue.vxml");
+
+        subdialogueCall.setMethod(SubmitMethod.post);
+
+        VoiceXmlInputTurn inputTurn = DialogueUtils.doTurn(subdialogueCall, context);
+
+        if (inputTurn.getJsonValue() != null) {
+            JsonObject subdialogueReturnValues = (JsonObject) inputTurn.getJsonValue();
+
+            for (Entry<String, JsonValue> returnValue : subdialogueReturnValues.entrySet()) {
+                context.getLogger().info("Subdialogue return value:"
+                                         + returnValue.getKey()
+                                         + "="
+                                         + returnValue.getValue());
+            }
+
+        }
+
         //end of dialogue
         return new Exit("exit");
     }
-
 }
