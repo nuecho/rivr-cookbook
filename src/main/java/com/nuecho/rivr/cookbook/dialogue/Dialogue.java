94d7f5347b741ed0ebbdf92046fe2719cffdc83c
Jean-Philippe Gariépy <Jean-Philippe.Gariepy@nuecho.com>
2014-03-06 08:28:33 -0500

    Added HTTP parameter extraction recipe.

--- src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
+++ src/main/java/com/nuecho/rivr/cookbook/dialogue/Dialogue.java
@@ -4,18 +4,33 @@
 
 package com.nuecho.rivr.cookbook.dialogue;
 
+import java.util.*;
+import java.util.Map.Entry;
+
+import com.nuecho.rivr.core.dialogue.*;
 import com.nuecho.rivr.voicexml.dialogue.*;
 import com.nuecho.rivr.voicexml.turn.first.*;
 import com.nuecho.rivr.voicexml.turn.last.*;
+import com.nuecho.rivr.voicexml.turn.output.*;
+import com.nuecho.rivr.voicexml.turn.output.audio.*;
 
 public class Dialogue implements VoiceXmlDialogue {
 
     @Override
-    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context)
-            throws Exception {
+    public VoiceXmlLastTurn run(VoiceXmlFirstTurn firstTurn, VoiceXmlDialogueContext context) throws Exception {
+
+        Map<String, String> parameters = firstTurn.getParameters();
+
+        List<AudioItem> audioItems = new ArrayList<AudioItem>();
+        for (Entry<String, String> entry : parameters.entrySet()) {
+            SpeechSynthesis speechSynthesis = new SpeechSynthesis("Parameter " + entry.getKey() + " has value " + entry.getValue() + ". ");
+            audioItems.add(speechSynthesis);
+        }
+
+        Message outputTurn = new Message("message", audioItems);
+        DialogueUtils.doTurn(outputTurn, context);
 
         //end of dialogue
         return new Exit("exit");
     }
-
 }
